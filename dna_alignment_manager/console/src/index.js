cde8280a7e4ce45ca0aac8856e290b6fbf97a354
Franklin van Nes <franklin.vannes@gmail.com>
2019-11-03 13:53:26 -0500

    Component landmark

--- dna_alignment_manager/console/src/index.js
+++ dna_alignment_manager/console/src/index.js
@@ -1,6 +1,6 @@
 import Console from './components/Console.tsx';
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 
 const wrapper = document.getElementById('app');
 wrapper ? ReactDOM.render(<Console />, wrapper) : null;
